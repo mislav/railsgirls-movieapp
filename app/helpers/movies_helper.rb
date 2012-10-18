a4573b83b0871a9424007f8e2e9b7de18a28c4cf
Mislav Marohnić <mislav.marohnic@gmail.com>
2012-10-19 00:45:03 +0200

    extract runtime, rating to helpers
    NOTE: encoding header

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,16 @@
+# encoding: utf-8
 module MoviesHelper
+  def display_duration value
+    hours = value / 60
+    minutes = value % 60
+
+    output = ''
+    output << "#{hours}h"      if hours > 0
+    output << " #{minutes}min" if minutes > 0
+    output
+  end
+
+  def display_rating value
+    "★" * value
+  end
 end
