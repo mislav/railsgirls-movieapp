d123e4eed328a9c08d230f8a4fc9f001c4fcc77b
Mislav Marohnić <mislav.marohnic@gmail.com>
2012-10-18 23:41:34 +0200

    movie.rb: add validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,7 @@
 class Movie < ActiveRecord::Base
   attr_accessible :rating, :title, :year
+
+  validates :title,  :presence     => true
+  validates :year,   :numericality => { :greater_than => 1890 }
+  validates :rating, :inclusion    => { :in => 1..5 }
 end
