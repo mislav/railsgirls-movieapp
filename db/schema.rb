e0bb6275cc7982213910c7c6fd4a6810e81c0bc9
Mislav Marohnić <mislav.marohnic@gmail.com>
2012-10-18 23:54:34 +0200

    $ rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121018212501) do
+ActiveRecord::Schema.define(:version => 20121018215351) do
 
   create_table "movies", :force => true do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20121018212501) do
     t.integer  "rating"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "runtime"
   end
 
 end
