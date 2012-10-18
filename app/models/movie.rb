882f0067fbbbdc3e1ea509adad7c8c4755e32a1b
Mislav Marohnić <mislav.marohnic@gmail.com>
2012-10-19 00:33:16 +0200

    allow editing of movie runtime
    NOTE: attr_accessible

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ActiveRecord::Base
-  attr_accessible :rating, :title, :year
+  attr_accessible :rating, :title, :year, :runtime
 
   validates :title,  :presence     => true
   validates :year,   :numericality => { :greater_than => 1890 }
